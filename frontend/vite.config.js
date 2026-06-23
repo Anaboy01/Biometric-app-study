6c533c17bba809a79f275ae1dfef728006bfdbd0
anaboy01 <aliyuanate016@gmail.com>
2026-06-23 21:01:24 +0100

    good

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://biometric-app-study-1.onrender.com",
+        target: "http://localhost:3002",
         changeOrigin: true,
       },
     },
